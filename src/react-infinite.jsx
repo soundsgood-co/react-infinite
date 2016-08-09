e38505d559c23728d72a383026611ef1ce4a79c4
Nicolas Dupont <npg.dupont@gmail.com>
2016-08-09 10:55:58 +0200

    Remove useless scrollbar

--- src/react-infinite.jsx
+++ src/react-infinite.jsx
@@ -217,8 +217,7 @@ var Infinite = React.createClass({
       utilities.buildScrollableStyle = () => {
         return Object.assign({}, {
           height: this.computedProps.containerHeight,
-          overflowX: 'hidden',
-          overflowY: 'scroll',
+          overflow: 'hidden',
           WebkitOverflowScrolling: 'touch'
         }, this.computedProps.styles.scrollableStyle || {});
       };
